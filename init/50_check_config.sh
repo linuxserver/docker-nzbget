c7eee1f8de053c04b76d092286ee46b3887fca1f
Stian Larsen <lonixx@gmail.com>
2015-08-18 10:22:36 +0200

    in bed with sed

--- init/50_check_config.sh
+++ init/50_check_config.sh
@@ -3,9 +3,18 @@
 if [ ! -f /config/nzbget.conf ]; then
   echo "No config found, copys default now"
   cp -v /app/nzbget.conf /config/nzbget.conf
-else
+  echo "Changeing some defaults to match our container"
+  sed -i -e "s#\(MainDir=\).*#\1/downloads#g" /config/nzbget.conf
+  sed -i -e "s#\(UMask=\).*#\1000#g" /config/nzbget.conf
+  sed -i -e "s#\(ScriptDir=\).*#\1$\{MainDir\}/scripts#g" /config/nzbget.conf
+  sed -i -e "s#\(ControlIP=\).*#\1#g" /config/nzbget.conf
   cp /tmp/nzbget.conf /config/
   chown abc:abc /config/nzbget.conf
   mkdir -p /downloads/dst
   chown -R abc:abc /downloads
 fi
+
+
+echo "Checking som config options"
+sed -i -e "s#\(WebDir=\).*#\1$\{AppDir\}/webui#g" /config/nzbget.conf
+sed -i -e "s#\(ConfigTemplate=\).*#\1$\{AppDir\}/webui/nzbget.conf.template#g" /config/nzbget.conf
